975838a797090df4017270c4b1306731e2625700
PhamIchChuyen220603 <chuyen.pi11316@sinhvien.hoasen.edu.vn>
2023-02-01 15:41:33 +0700

    D02-Garena

---
+++ script/index.js
@@ -0,0 +1,35 @@
+// //alert("Hello Word, this is a JavaScript")
+
+// let myname = "Tèo"
+// console.log(myname);
+// let a =8
+// let b =10
+// let c = a+b
+// console.log(c)
+// let arr = [1,2,3,4,5]
+// arr.push(100)
+// arr.pop()
+// arr.push(200)
+// console.log(arr)
+// console.log( c + arr)
+
+let input = prompt("Nhập số bí ẩn")
+console.log(input)
+
+let output = prompt("Đoán số bí ẩn")
+console.log(output)
+
+if (input == output) {
+    console.log("Bạn đã nhập đúng");
+
+} else {
+    if (input != output) {
+        console.log("Bạn đã nhập sai")
+    }
+    
+}
+
+
+
+
+
